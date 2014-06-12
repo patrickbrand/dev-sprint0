603068be07f8706f4f507d4855855077a43d258a
Patrick Brand <patrick.brand@gmail.com>
2014-06-12 00:00:14 -0400

    Turtle World stuff

--- swampy-package/swampy/sib_hello.py
+++ swampy-package/swampy/sib_hello.py
@@ -1,18 +1,87 @@
 # Hello excercise from Week 0
 
-# Name:
+# Name: Patrick Brand
 
 
 from TurtleWorld import * 		
 world = TurtleWorld()			
-bob = Turtle()				
-
-
+bob = Turtle()			
 
 # This is where you put code to move bob
 
+# H
 
+lt(bob)
+lt(bob)
+pu(bob)
+fd(bob, 150)
+pd(bob)
+rt(bob)
+fd(bob, 100)
+bk(bob, 50)
+rt(bob)
+fd(bob, 30)
+lt(bob)
+fd(bob, 50)
+lt(bob)
+lt(bob)
+fd(bob, 100)
+bk(bob, 50)
+lt(bob)
+pu(bob)
 
+# E
+
+fd(bob, 30)
+pd(bob)
+lt(bob)
+fd(bob, 50)
+rt(bob)
+fd(bob, 30)
+bk(bob, 30)
+rt(bob)
+fd(bob, 50)
+lt(bob)
+fd(bob, 30)
+bk(bob, 30)
+rt(bob)
+fd(bob, 50)
+lt(bob)
+fd(bob, 30)
+pu(bob)
+
+# L
+
+fd(bob, 30)
+pd(bob)
+fd(bob, 30)
+bk(bob, 30)
+lt(bob)
+fd(bob, 100)
+rt(bob)
+pu(bob)
+
+# L
+
+fd(bob, 50)
+pd(bob)
+rt(bob)
+fd(bob, 100)
+lt(bob)
+fd(bob, 30)
+pu(bob)
+
+# O
+
+fd(bob, 30)
+pd(bob)
+fd(bob, 30)
+lt(bob)
+fd(bob, 100)
+lt(bob)
+fd(bob, 30)
+lt(bob)
+fd(bob, 100)
 
 
 
--- swampy-package/swampy/sib_polygon.py
+++ swampy-package/swampy/sib_polygon.py
@@ -1,8 +1,8 @@
 # Polygon excercise from Week 0
 
-# Name:
+# Name: Patrick Brand
 
-
+import math
 from TurtleWorld import * 		
 world = TurtleWorld()			
 bob = Turtle()				
@@ -11,11 +11,29 @@ bob = Turtle()
 
 # This is where you put code to move bob
 
+def polygon(turtle, length, n):
+    for i in range(int(n)):
+        angle = 360 / n
+        fd(turtle, length)
+        lt(turtle, angle)
+        
+# polygon(bob, 100, 6)
 
+def circle(turtle, r):
+    circumference = math.pi * (2 * r)
+    print 'circumference is ' + str(circumference)
+    polygon(turtle, 10, circumference)
+    
+# circle(bob, 5)
 
+def arc(turtle, r, theta):
+# circumference = math.pi * (2 * r)
+    length = 1
+    for i in range(int(theta)):
+        angle = 360 / theta
+        fd(turtle, length)
+        lt(turtle, angle)
 
-
-
-
+arc(bob, 5, 360)
 
 wait_for_user()					
